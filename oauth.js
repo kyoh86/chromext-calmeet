4d32072a65cd336a749a56f4b683019571f1767d
Kyoichiro Yamada <me@kyoh86.dev>
2020-06-19 05:55:42 +0900

    get current event and search attendees

--- oauth.js
+++ oauth.js
@@ -4,27 +4,54 @@
 
 'use strict';
 
+const meetID = 'eag-qzsw-xem';
 window.onload = function() {
   document.querySelector('button').addEventListener('click', function() {
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-      const calendarId = 'primary';
+    chrome.identity.getAuthToken({interactive: true}, async function(token) {
       const pre = document.getElementById('events-tryout');
-      let init = {
-        method: 'GET',
-        async: true,
-        headers: {
-          Authorization: 'Bearer ' + token,
-          'Content-Type': 'application/json'
-        },
-        'contentType': 'json'
-      };
-      fetch(
-          `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
-          init)
-          .then((response) => response.json())
-          .then(function(data) {
-            pre.innerText = JSON.stringify(data, null, 2);
-          });
+      let events = await fetchEvents(token);
+      if (!events) {
+        return;
+      }
+
+      let attendee = events
+        .filter(e => !!e.hangoutLink && e.hangoutLink.endsWith('/' + meetID))
+        .flatMap(e => e.attendees.map(a => a.email));
+      pre.innerText = attendee.join("\n");
     });
   });
 };
+
+async function fetchEvents(token) {
+  const calendarId = 'primary';
+  let init = {
+    method: 'GET',
+    async: true,
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    },
+    'contentType': 'json'
+  };
+
+  let query = new URLSearchParams();
+  let now = new Date();
+
+  let max = new Date(now);
+  max.setHours(max.getHours() + 1); // an hour later
+  query.append('timeMax', max.toISOString());
+
+  let min = new Date(now);
+  min.setHours(min.getHours() - 1); // an hour before
+  query.append('timeMin', min.toISOString());
+
+  console.debug(`get events from ${min.toLocaleString()} to ${max.toLocaleString()}`);
+
+  let url = 'https://www.googleapis.com/calendar/v3/calendars/'
+          + calendarId
+          + '/events'
+          + '?' + query.toString();
+
+  let res = await fetch(url, init).then(res => res.json());
+  return res.items;
+}
